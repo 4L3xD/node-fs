2ef207716e13b90efd5e2f571970df9a9884dbbb
Alessandra Oliveira <alessandra@prontocombustiveis.com.br>
2020-01-23 17:29:25 -0300

    simplify

--- readJson.js
+++ readJson.js
@@ -1,16 +1,11 @@
-var fs = require("fs");
+let fs = require("fs");
+
 fs.readFile("./cof-data-list/2020.json" , "utf8", function(err, data){
   if(err){
     return console.log("Erro ao ler arquivo");
   }
   
-  var jsonData = JSON.parse(data);
+  let jsonData = JSON.parse(data);
   console.log(jsonData)
- //  Se precisar em array use:
- // jsonData = Object.keys(jsonData);
 
 });
-
-// var jsonData = fs.readFileSync('./cof-data-list/2020.json', 'utf8');
-
-// console.log(jsonData)
